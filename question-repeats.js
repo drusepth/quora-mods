0caf8f8f0ef77f8893bc0ed7493467b19f75d156
Andrew Brown <drusepth@gmail.com>
2017-03-13 20:35:12 +0000

    Automatically suggest edits removing questions quoted at the top of answers

--- question-repeats.js
+++ question-repeats.js
@@ -1,5 +1,41 @@
+function click_submit_button_later(answer_dom) {
+  setTimeout(function () {
+    console.log("Submitting edit suggestion to remove quoting the question.")
+    //answer_dom.getElementsByClassName('submit_button')[0].click();
+  }, 500);
+};
 
-var question_text = $('.question_text_edit').innerText.trim();
+function remove_question_text_from_answer(answer_dom) {
+  setTimeout(function () {
+    var doc_dom = answer_dom.getElementsByClassName('doc')[0];
+    doc_dom.innerHTML = doc_dom.innerHTML.replace(question_text, '');
+    
+    // Write explanation for edit:
+    answer_dom.getElementsByClassName('doc')[1].innerText = "It's unnecessary to repeat the question text at the top of your question.";
+    
+    // Click the submit button
+    click_submit_button_later(answer_dom);
+  }, 1000);
+};
+
+function suggest_edits_for_answer_dom(answer_dom) {
+  // This is wrapped in a setTimeout to give the lazy-loading menu time to load
+  setTimeout(function () {
+    // 2. Click the suggest edits link
+    var suggest_edits_button = answer_dom.getElementsByClassName('menu_list_item')[4].getElementsByTagName('a')[0];
+    if (suggest_edits_button.innerText == 'Suggestions Pending') {
+      console.log("Skipping suggesting edits for answer since they're still pending.");
+      return;
+    } else {
+      suggest_edits_button.click();
+    }
+
+    // Make the edit and submit it
+    remove_question_text_from_answer(answer_dom);
+  }, 500);
+};
+
+var question_text = document.getElementsByClassName('question_text_edit')[0].innerText.trim();
 var answers = document.getElementsByClassName('AnswerPagedList')[0].getElementsByClassName('pagedlist_item');
 for (i = 0; i < answers.length - 1; i++) {
   var answer_dom = answers[i];
@@ -18,23 +54,5 @@ for (i = 0; i < answers.length - 1; i++) {
   // 1. Load the lazy-loaded overflow menu
   answer_dom.getElementsByClassName('overflow_link')[1].click();
   
-  // 2. Click the suggest edits link
-  var suggest_edits_button = answer_dom.getElementsByClassName('menu_list_item')[4].getElementsByTagName('a')[0];
-  if (suggest_edits_button.innerText == 'Suggestions Pending') {
-    console.log("Skipping suggesting edits for answer " + (i + 1) + " since they're still pending.");
-    continue;
-  } else {
-    suggest_edits_button.click();
-  }  
-  
-  // Make the edit
-  var doc_dom = answer_dom.getElementsByClassName('doc')[0];
-  doc_dom.innerHTML = doc_dom.innerHTML.replace(question_text, '');
-  
-  // Write explanation for edit:
-  answer_dom.getElementsByClassName('doc')[1].innerText = "It's unnecessary to repeat the question text at the top of your question.";
-  
-  // Click the submit button
-  console.log("Suggesting edits for " + (i + 1) + ": " + answer_text);
-  //answer_dom.getElementsByClassName('submit_button')[0].click();
+  suggest_edits_for_answer_dom(answer_dom);
 }
